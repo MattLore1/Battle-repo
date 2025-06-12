cbddda1470a04ce2c9fe833c5fe1f53b8a3ad34a
Matthew LoRe <Mlore@rationalenterprise.com>
2025-06-12 13:23:47 -0400

    add python script

---
+++ dnd.py
@@ -0,0 +1,114 @@
+ '''###TODO: Allow for character selection and naming 
+ '''
+
+
+
+import random
+class Mortal:
+    def __init__(self, name, armor_class, health):
+        self.name = name
+        self.armorClass = armor_class
+        self.health = health
+        self.max_health = health
+        self.health_potion_uses = 3
+
+    def is_alive(self):
+        return self.health > 0
+    
+    def get_health_state(self):
+        return f"({self.health}/{self.max_health})"
+    
+    def receive_attack(self, attack_roll, damage_roll):
+        if attack_roll <= self.armorClass:
+            print(self.name, "blocked your attack roll of", attack_roll)
+        else:
+            self.health = self.health - damage_roll
+            print(self.name, "took", damage_roll, "damage", self.get_health_state())
+            if attack_roll == 20:
+                print(self.name, "took", damage_roll, "extra critical hit damage", self.get_health_state())
+                self.health = self.health - damage_roll
+            if not self.is_alive():
+                print(self.name, "died")
+
+    def attack(self, target):
+        if not self.is_alive():
+            print(self.name, "is dead and cannot attack.")
+        return
+
+    attack_roll = roll_d20()
+    damage_roll = roll_d10()
+    target.receive_attack(attack_roll, damage_roll)
+    print(self.name, "attacks", target.name, "with a roll of", attack_roll)
+    target.receive_attack(attack_roll, damage_roll)
+
+    def health_potion(self):
+        if self.is_alive() and self.health <= (1/2 * self.max_health):
+            if self.health_potion_uses > 0:
+                self.health += random.randint(1, 4) + 2
+                self.health_potion_uses -= 1
+                print(self.name, "drank a health potion and restored health", self.get_health_state())
+            else:
+                print(self.name, "has no health potions left")
+
+
+# function to roll a d20
+def roll_d20():
+    return random.randint(1, 20)
+def roll_d10():
+    return random.randint(1, 10)
+
+enemies = [
+    Mortal("Goblin Archer 1", 12, 12),
+    Mortal("Goblin Archer 2", 12, 12),
+    Mortal("Goblin Archer 3", 12, 12),
+    Mortal("Goblin Boss", 14, 30)
+]
+
+players = [
+    Mortal("Barbarian", 16, 40),
+    Mortal("Wizard", 13, 25)
+]
+
+while len(enemies) > 0 and len(players) > 0:
+
+    for player in players:
+        enemy = random.choice(enemies)
+
+        player.attack(enemy)
+        if not enemy.is_alive():
+            enemies.remove(enemy)
+
+    for enemy in enemies:
+        player = random.choice(players)
+
+        enemy.attack(player)
+        if not player.is_alive():
+            players.remove(player)
+
+if len(players) > 0:
+    print("Players win!")
+else:
+    print("Monsters win!")
+
+
+while len(enemies) > 0 and len(players) > 0:
+
+    for player in players:
+        enemy = random.choice(enemies)
+
+        player.attack(enemy)
+        if not enemy.is_alive():
+            enemies.remove(enemy)
+
+    for enemy in enemies:
+        player = random.choice(players)
+
+        enemy.attack(player)
+        if not player.is_alive():
+            players.remove(player)
+
+
+if len(players) > 0:
+    print("Players win!")
+else:
+    print("Monsters win!")
